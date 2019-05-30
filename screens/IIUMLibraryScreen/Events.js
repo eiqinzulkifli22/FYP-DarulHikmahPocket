1bba63627a16e8fc4e70cf20212f7c2266ea0774
eiqinzulkifli22 <45284883+eiqinzulkifli22@users.noreply.github.com>
2019-05-30 15:36:03 +0800

    Update Events.js

--- screens/IIUMLibraryScreen/Events.js
+++ screens/IIUMLibraryScreen/Events.js
@@ -1,57 +1,43 @@
 import React from 'react';
-import {View, Text,StyleSheet} from 'react-native';
-import { Header} from 'react-native-elements';
+import { View, Text, StyleSheet } from 'react-native';
+import { Header } from 'react-native-elements';
 import '@expo/vector-icons';
+import { HeaderBackButton } from 'react-navigation';
 
 
-class Events extends React.Component{
-  static navigationOptions = ({navigation}) => ({
-    title: 'Events'
+class Events extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'EVENTS',
+    headerLeft: (
+      <HeaderBackButton
+        onPress={() => { navigation.navigate('IIUMLibScreen') }} />
+    )
   })
-  
-    render(){
-        return(
 
+  render() {
+    return (
 
-            <View style={styles.header}>
-       {/* <Header
-          leftComponent={{
-            icon: 'menu',
-            color: '#fff',
-            onPress: () => {this.props.navigation.openDrawer()},
-          }}
-          
-          rightComponent={{ 
-            icon: 'person', 
-            color: '#fff',
-            onPress: () => this.props.navigation.navigate('My Account')}}
-          centerComponent={{  style: { color: '#fff' } }}
-          backgroundColor="#028A7E"
-        /> */}
+      <View style={styles.header}>
+        <View style={styles.container}>
 
-            <View style={styles.container}>  
-            
-                    <Text>No recent events</Text>
+          <Text>No recent events.</Text>
 
-            </View>
-
-        </View> 
-      
-     
-        )
-    }
+        </View>
+      </View>
+    )
+  }
 }
 export default Events;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    header: {
-        flex: 1,
-        paddingTop: 4,
-        backgroundColor: '#ecf0f1',
-      }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  header: {
+    flex: 1,
+    paddingTop: 4,
+    backgroundColor: '#ecf0f1',
+  }
 })

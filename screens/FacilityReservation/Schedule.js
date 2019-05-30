2fc9868d508728e90cb08374397ea6b5f1b0f524
eiqinzulkifli22 <45284883+eiqinzulkifli22@users.noreply.github.com>
2019-05-30 15:34:34 +0800

    Update Schedule.js

--- screens/FacilityReservation/Schedule.js
+++ screens/FacilityReservation/Schedule.js
@@ -1,78 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button} from 'react-native';
-import { Icon,  Container, Content, Left } from 'native-base'
-import {Header, SearchBar } from 'react-native-elements';
-import {createStackNavigator, createAppContainer, createDrawerNavigator, createSwitchNavigator,  createNavigationContainer} from 'react-navigation';
-//import OpeningHoursSreen from './screens/OpeningHoursSreen';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import '@expo/vector-icons';
 
-
-
-
-class Schedule extends React.Component{
-
-  static navigationOptions = ({navigation}) => ({
+class Schedule extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
     title: 'Schedule'
-    
   })
+  render() {
+    return (
+      <View style={styles.header}>
 
-   /*  static navigationOptions = {
-        headerTitle: <HomeScreen/>,
-        headerLeft: (
-          <Button
-            onPress={() => alert('This is a button!')}
-            title="Info"
-            color="#fff"
-          />
-        ),
-      }; */
-
-    render(){
-        return(
-
-          
-             <View style={styles.header}>
-     {/*    <Header
-        
-          leftComponent={{
-            icon: 'menu',
-            color: '#fff',
-            onPress: () => {this.props.navigation.openDrawer()},
-          }}
-          
-          rightComponent={{ 
-            icon: 'person', 
-            color: '#fff',
-            onPress: () => this.props.navigation.navigate('My Account')}}
-          centerComponent={{  style: { color: '#fff' } }}
-          backgroundColor="#028A7E"
-        /> 
- */}
-          <View style={styles.container}>  
-            
-              <Text>Schedule</Text>
-
-              
-            
-          </View>
-        </View>  
-        )
-    }
+        <View style={styles.container}>
+          <Text>Schedule</Text>
+        </View>
+      </View>
+    )
+  }
 }
 
 
 export default Schedule;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 
-    header: {
-        flex: 1,
-        paddingTop: 4,
-        backgroundColor: '#ecf0f1',
-      }
+  header: {
+    flex: 1,
+    paddingTop: 4,
+    backgroundColor: '#ecf0f1',
+  }
 })  

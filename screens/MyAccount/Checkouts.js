010622eb4a3e1b8267a4fee029cd7861dd01eff7
eiqinzulkifli22 <45284883+eiqinzulkifli22@users.noreply.github.com>
2019-05-30 15:49:08 +0800

    Update Checkouts.js

--- screens/MyAccount/Checkouts.js
+++ screens/MyAccount/Checkouts.js
@@ -1,81 +1,175 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button} from 'react-native';
-import { Icon,  Container, Content, Left } from 'native-base'
-import {Header, SearchBar } from 'react-native-elements';
-import {createStackNavigator, createAppContainer, createDrawerNavigator, createSwitchNavigator,  createNavigationContainer} from 'react-navigation';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, FlatList, TouchableOpacity, Alert, Image } from 'react-native';
+import { env } from 'config/env.js';
+import { SecureStore } from 'expo';
+import { HeaderBackButton } from 'react-navigation';
 
-import '@expo/vector-icons';
+let userToken = "";
 
-
-
-
-class Checkouts extends React.Component{
-
+export default class Hold extends Component {
   static navigationOptions = ({navigation}) => ({
-    title: 'Checkouts'
-    
-  })
-  
-
-   /*  static navigationOptions = {
-        headerTitle: <HomeScreen/>,
-        headerLeft: (
-          <Button
-            onPress={() => alert('This is a button!')}
-            title="Info"
-            color="#fff"
-          />
-        ),
-      }; */
-
-    render(){
-        return(
-
-          
-
-             <View style={styles.header}>
-     {/*    <Header
-        
-          leftComponent={{
-            icon: 'menu',
-            color: '#fff',
-            onPress: () => {this.props.navigation.openDrawer()},
-          }}
-          
-          rightComponent={{ 
-            icon: 'person', 
-            color: '#fff',
-            onPress: () => this.props.navigation.navigate('My Account')}}
-          centerComponent={{  style: { color: '#fff' } }}
-          backgroundColor="#028A7E"
-        />  */}
-
-          <View style={styles.container}>  
-            
-              <Text>Checkouts</Text>
-
-              
-              
-
-          </View>
-        </View>  
+    title:'RENEW',
+    headerLeft:(
+      <HeaderBackButton 
+        onPress={()=>{navigation.navigate('MyAccountScreen')}}/>
         )
+  }) 
+  constructor() {
+    super()
+    this.state = {
+      dataSource: [],
+      searchTerm: ""
     }
+  }
+
+  search = (term) => {
+    fetch(env('LARAVEL_HOST') + '/api/loans/onLoanDetails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + userToken,
+      },
+      body: JSON.stringify({
+        term: term,
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // console.log("Retrieve:", responseJson);
+
+        if (responseJson.length == 0) {
+         // Alert.alert("Result not found");
+        }
+
+        this.setState({
+          dataSource: responseJson
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  renderItem = (item, index) => {
+    // console.log("Render item:", item);
+    let currentItem = item.item;
+    return (
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('RenewBook', {'currentItem': currentItem})} >
+        <Image source={{uri: currentItem.book_copy.book.cover_page_url}} style={styles.bookImage}/>
+          <View style={{ flex: 1, justifyContent: 'center' }} style={styles.bookdetails}>
+          <Text>
+            {currentItem.book_copy.copy_no}
+            </Text>
+            <Text >
+            {currentItem.book_copy.book.title}
+            </Text>
+            <Text>
+            {currentItem.book_copy.book.author}
+            </Text>
+            <Text></Text> 
+            <Text style={{color: 'red'}}>
+            Due Date:  {currentItem.formatted_due_date}
+            </Text> 
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  keyExtractor = (item, index) => {
+    return index.toString();
+  };
+
+  componentDidMount() {
+    SecureStore.getItemAsync('user_token')
+      .then((token) => {
+        userToken = token;
+        this.search('');
+      })
+  }
+
+  FlatListItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+        }}
+      />
+    );
+  }
+
+  render() {
+    const { searchTerm } = this.state;
+    return (
+      <View style={styles.MainContainer}>
+
+        <FlatList
+          icon style={styles.liststyle}
+          data={this.state.dataSource}
+          ItemSeparatorComponent={this.FlatListItemSeparator}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+          height={20}
+        />
+      </View>
+    )
+  }
 }
 
-
-export default Checkouts;
-
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
 
-    header: {
-        flex: 1,
-        paddingTop: 4,
-        backgroundColor: '#ecf0f1',
-      }
-})  
+  MainContainer: {
+
+    // Setting up View inside content in Vertically center.
+    justifyContent: 'center',
+    flex: 1,
+    padding: 5,
+
+  },
+
+  textViewContainer: {
+
+    textAlignVertical: 'center',
+    padding: 10,
+    fontSize: 20,
+    color: '#fff',
+
+  },
+
+  TextInputStyleClass: {
+    textAlign: 'center',
+    alignItems: 'center',
+    marginBottom: 15,
+    height: 35,
+    width: 300,
+    borderRadius: 10,
+    borderWidth: 1,
+    color: '#028a7e',
+  },
+
+  liststyle: {
+    backgroundColor: "white",
+    color: "black",
+    fontSize: 18,
+    marginTop: 20,
+  },
+
+   bookdetails: {
+    left: 75,
+    marginBottom: 50,
+    marginTop: 10,
+  },
+  
+  bookImage: {
+    position: 'absolute',
+    left: 5,
+    height: 75,
+    width: 60,
+    marginTop: 10,
+  }
+
+});
+
+
